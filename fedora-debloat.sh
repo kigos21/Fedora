4e13f83a8f1a28bfaf655107be761e590ffc45c4
kigos21 <decastrojohndaniel@gmail.com>
2024-06-06 18:17:26 +0800

    update settings

--- fedora-debloat.sh
+++ fedora-debloat.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Debloat
-# dnf remove gnome-boxes gnome-calendar cheese cheese-libs gnome-contacts mediawriter gnome-maps gnome-photos rhythmbox gnome-system-monitor gnome-tour gnome-weather -y
-dnf remove gnome-boxes gnome-calendar gnome-contacts mediawriter gnome-maps gnome-photos rhythmbox gnome-system-monitor gnome-tour gnome-weather -y
+dnf remove gnome-calendar gnome-contacts gnome-weather gnome-maps mediawriter gnome-system-monitor gnome-boxes rhythmbox gnome-tour -y
